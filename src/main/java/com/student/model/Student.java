be48681bf3857e98bcba17a43b8af8017212216f
vasu6369 <www.gvasu2005@gmail.com>
2025-05-29 23:35:15 +0530

    student bean class created

--- src/main/java/com/student/model/Student.java
+++ src/main/java/com/student/model/Student.java
@@ -1,4 +1,15 @@
 package com.student.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "student")
 public class Student {
+    private String id;
+    private String name;
+    private String email;
+    private int age;
 }
