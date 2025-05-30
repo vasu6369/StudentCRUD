62f094af7e4ab095375f63a26e841023940dde64
vasu6369 <www.gvasu2005@gmail.com>
2025-05-30 17:20:35 +0530

    get student by id feature implemented

--- src/main/java/com/student/service/StudentService.java
+++ src/main/java/com/student/service/StudentService.java
@@ -3,8 +3,11 @@ package com.student.service;
 import com.student.model.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
     public Student createStudent(Student student);
     public List<Student> getAllStudents();
+    public Optional<Student> getStudentById(String id);
+
 }
--- src/main/java/com/student/service/StudentServiceImpl.java
+++ src/main/java/com/student/service/StudentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService{
@@ -23,5 +24,12 @@ public class StudentServiceImpl implements StudentService{
     public List<Student> getAllStudents(){
         return studentrespository.findAll();
     }
+
+    @Override
+    public Optional<Student> getStudentById(String id){
+        return studentrespository.findById(id);
+    }
+
+
 
 }
